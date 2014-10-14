39879cf9b833a7c7f4c63dbfe6c4185225992888
Wayne Wang <lastr2d2@gmail.com>
2014-10-14 23:48:09 +0800

    add problem for reverse_words_in_a_string

---
+++ leetcode/leetcode/02_reverse_words_in_a_string.cpp
@@ -0,0 +1,32 @@
+/*
+	https://oj.leetcode.com/problems/reverse-words-in-a-string/
+
+	Reverse Words in a String
+
+	Given an input string, reverse the string word by word.
+
+	For example,
+	Given s = "the sky is blue",
+	return "blue is sky the".
+
+	Clarification:
+	What constitutes a word?
+	A sequence of non-space characters constitutes a word.
+	Could the input string contain leading or trailing spaces?
+	Yes. However, your reversed string should not contain leading or trailing spaces.
+	How about multiple spaces between two words?
+	Reduce them to a single space in the reversed string.
+*/
+
+#include<string>
+#include "02_reverse_words_in_a_string.h"
+
+using namespace std;
+void reverseWords(string &s) {
+	
+
+}
+
+void reverseWords_Test(){
+
+}
